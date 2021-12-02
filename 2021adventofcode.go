61cc5c4abe4777db6a301711e2622b97f2ca734e
Andrew Shoell <mrlunchbox777@gmail.com>
2021-12-01 22:15:46 -0700

    got day 1

--- 2021adventofcode.go
+++ 2021adventofcode.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"log"
 	"bufio"
+	"strconv"
 )
 
 func main() {
@@ -17,10 +18,27 @@ func main() {
 	}
 	defer file.Close()
 
+	totalIncreases := int(0)
+	currentDepth := 0
+	firstRun := true
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		i, _ := strconv.Atoi(scanner.Text())
+		increased := false
+		if firstRun {
+			firstRun = false
+		} else if i > currentDepth {
+			increased = true
+			totalIncreases++
+		}
+		currentDepth = i
+		if increased {
+			fmt.Println(currentDepth, "(increased)")
+		} else {
+			fmt.Println(currentDepth, "(decreased)")
+		}
 	}
+	fmt.Println("Increases", totalIncreases)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
