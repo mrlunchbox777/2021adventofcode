40fe69a467fe33d82596b0149ad90cdc78bcabaa
Andrew Shoell <mrlunchbox777@gmail.com>
2021-12-04 22:04:30 -0700

    adding the starting state for day3

---
+++ src/day3/run.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"bufio"
+)
+
+func main() {
+	file, err := os.Open("./data/input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	x := 0
+	y := 0
+	aim := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		i := scanner.Text()
+}
