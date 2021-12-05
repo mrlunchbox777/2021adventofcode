b937261063430fbb97159d46c1b3362893d58498
Andrew Shoell <mrlunchbox777@gmail.com>
2021-12-04 22:44:15 -0700

    WIP: interesting point to pause for a save

--- src/day3/run.go
+++ src/day3/run.go
@@ -3,20 +3,34 @@ package main
 import (
 	"fmt"
 	"os"
-	"log"
 	"bufio"
+	// "strconv"
+	// "strings"
 )
 
 func main() {
 	file, err := os.Open("./data/input")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer file.Close()
 
+	// gamma := make([]map[int]int, 0)
+	// epsilon := make(map[int]int)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		i := scanner.Text()
-		fmt.Println("i -", i)
+		chars := []rune(i)
+		ints := []int{}
+		for _, currentChar := range chars {
+			currentInt, err := int(currentChar)
+			if err != nil {
+				panic(err)
+			}
+			ints = append(ints, currentInt)
+		}
+			// find number of occurrences of each value in string and put into epsilon map
+			// for each char add to corresponding map in gamma for each value
+		fmt.Println("i -", i, "ints -", ints)
 	}
 }
