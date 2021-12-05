e9ebd52ddd4820c25703734d656f5d2c3d7bb774
Andrew Shoell <mrlunchbox777@gmail.com>
2021-12-04 18:13:51 -0700

    making a copy to track part 1

---
+++ src/day2/run-part1.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"bufio"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("./data/input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	x := 0
+	y := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		i := scanner.Text()
+		i_array := strings.Split(i, " ")
+		direction := i_array[0]
+		value, _ := strconv.Atoi(i_array[1])
+		fmt.Println("direction", direction, "- value", value)
+		switch direction {
+			case "up":
+				y -= value
+			case "down":
+				y += value
+			case "forward":
+				x += value
+		}
+		fmt.Println("x", x, "- y", y)
+	}
+	final_product := x * y
+	fmt.Println("final_product", final_product)
+}
