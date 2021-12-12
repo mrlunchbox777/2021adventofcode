685e3776231da61eb0b86bd66c2535e1c19079cc
Andrew Shoell <mrlunchbox777@gmail.com>
2021-12-12 15:47:31 -0700

    WIP: trying a different way of doing packages so that exporting can work

--- 2021-advent-of-code.go
+++ 2021-advent-of-code.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/mrlunchbox777/2021adventofcode/src/day1/part1"
 )
 
 func main() {
 	fmt.Println("test")
+	day1part1.main()
 }
--- src/day1/part1/run.go
+++ src/day1/part1/run.go
@@ -1,4 +1,4 @@
-package day1-part1
+package day1part1
 
 import (
 	"bufio"
