5176348d470ba44243a6734d3ec7add60149b3ad
Andrew Shoell <mrlunchbox777@gmail.com>
2021-12-12 18:02:40 -0700

    WIP: I think I got day selection good

--- 2021-advent-of-code.go
+++ 2021-advent-of-code.go
@@ -7,11 +7,12 @@ import (
 	// d2p2 "github.com/mrlunchbox777/2021adventofcode/src/day2/part2"
 	// d3p1 "github.com/mrlunchbox777/2021adventofcode/src/day3/part1"
 	// d3p2 "github.com/mrlunchbox777/2021adventofcode/src/day3/part2"
-	d4p1 "github.com/mrlunchbox777/2021adventofcode/src/day4/part1"
+	// d4p1 "github.com/mrlunchbox777/2021adventofcode/src/day4/part1"
 	"io/ioutil"
 	// "os"
 	"regexp"
 	"sort"
+	"strconv"
 )
 
 func IOReadDir(root string) ([]string, error) {
@@ -28,31 +29,44 @@ func IOReadDir(root string) ([]string, error) {
 }
 
 func main() {
-	regGetNum := regexp.MustCompile(`[0-9]+`)
-	regNo := regexp.MustCompile(`^[Nn][Oo]*$`)
-	// regNum := regexp.MustCompile(`^[0-9]+$`)
+	regGetNum := regexp.MustCompile("[0-9]+")
+	regYes := regexp.MustCompile("^[Yy][Ee]*[Ss]*$")
 
-	var dayString string
+	dayDirs, err := IOReadDir("./src")
+	if err != nil {
+		panic(err)
+	}
+	sort.Strings(dayDirs)
+	day, err := strconv.Atoi(regGetNum.FindString(dayDirs[len(dayDirs) - 1]))
+	if err != nil {
+		panic(err)
+	}
+	regDay := regexp.MustCompile("^[0-" + strconv.Itoa(day) + "]+$")
+
+	var lastestDayString string
 	fmt.Println("Use Latest Day?")
-	fmt.Scanln(&dayString)
-	if regNo.MatchString(dayString) {
-		// do check for days
-		// day := jkfdls;
-	} else {
-		dayDirs, err := IOReadDir("./src")
-		if err != nil {
-			panic(err)
+	fmt.Scanln(&lastestDayString)
+	if !regYes.MatchString(lastestDayString) {
+		var dayString string
+		fmt.Println("Which Day?")
+		fmt.Scanln(&dayString)
+		fmt.Println("dayString -" + dayString + "-")
+		if regDay.MatchString(dayString) {
+			day, err = strconv.Atoi(dayString)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			panic("Error: Bad Day Selection")
 		}
-		sort.Strings(dayDirs)
-		day := regGetNum.FindString(dayDirs[len(dayDirs) - 1])
-		fmt.Println("day - " + day)
 	}
 
-	var partString string
-	fmt.Println("Use Latest Part?")
-	fmt.Scanln(&partString)
+	// var partString string
+	// fmt.Println("Use Latest Part?")
+	// fmt.Scanln(&partString)
 
 
-	// fmt.Println("Running For " + )
-	d4p1.Main()
+	fmt.Println("Running for day", day)
+	// d4p1.Main()
+	fmt.Println("Ran for day", day)
 }
