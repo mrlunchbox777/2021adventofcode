9f4d1c09563a8a1cf6b9a0c893ab0a51cbdf38b3
Andrew Shoell <mrlunchbox777@gmail.com>
2021-12-24 23:18:53 -0700

    cleaning up the comments

--- src/day4/part1/run.go
+++ src/day4/part1/run.go
@@ -18,16 +18,11 @@ func Main() (error) {
 	if err != nil {
 		return err
 	}
-	// d4.PrintBingoBoards(bingoGame, false)
-	// d4.PrintWinningNumbers(bingoGame)
 
 	bingoGame, err = d4.CalcGame(bingoGame)
 	if err != nil {
 		return err
 	}
-
-	// d4.PrintBingoBoards(bingoGame, true)
-	d4.PrintWinningNumbers(bingoGame)
 
 	err = d4.PrintResults(bingoGame)
 	if err != nil {
